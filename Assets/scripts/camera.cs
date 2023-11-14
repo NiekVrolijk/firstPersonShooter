1b43b7e7aaacdbdc1629cb88ff7539b7b734b992
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-14 14:56:41 +0100

    help2

--- Assets/scripts/camera.cs
+++ Assets/scripts/camera.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
+    //var
+
+    //float maxXValue = 8.6f;
+    //float maxYValue = 4.7f;
+    [SerializeField]
+    private float currentSpeed;
+    public float walkingSpeed = 5;
+    public float runningSpeed = 10;
+
+
+
+    public CharacterController characterController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,25 @@ public class camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        //if (Input.GetKey(KeyCode.W) /*&& transform.position.y < maxYValue*/)
+        //{
+
+        //    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.S) /*&& transform.position.y > -maxYValue*/)
+        //{
+
+        //    transform.Translate(Vector3.back * speed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.D) /*&& transform.position.x < maxXValue*/)
+        //{
+        //    transform.Translate(Vector3.right * speed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.A) /*&& transform.position.x > -maxXValue*/)
+        //{
+        //    transform.Translate(Vector3.left * speed * Time.deltaTime);
+        //}
     }
 }
