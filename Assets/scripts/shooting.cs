b6243f6fe8c7506459565a1bd2e6f2f1d0548be1
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-12-04 16:29:50 +0100

    just working on gun but it won't work
    imme stop for today cuz i aint going anywhere with this

--- Assets/scripts/shooting.cs
+++ Assets/scripts/shooting.cs
@@ -14,6 +14,13 @@ public class shooting : MonoBehaviour
 
     private float shootTimer;
     private float canShoot = 0.2f;
+
+    public GameObject muzzleFlash;
+    private float muzzleFlashX;
+    private float muzzleFlashY;
+    private float muzzleFlashZ;
+    private float flashTime = 1f;
+    private float flashTimer;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,9 +32,19 @@ public class shooting : MonoBehaviour
     {
         shootTimer += Time.deltaTime;
 
+        //flashTimer += Time.deltaTime;
+        //muzzleFlash.transform.localScale = new Vector3(muzzleFlashX, muzzleFlashY, muzzleFlashZ);
+
+        //if (flashTime >= flashTimer)
+        //{
+        //    muzzleFlashX = 0f; muzzleFlashY = 0f; muzzleFlashZ = 0f;
+        //}
+
         if(Input.GetMouseButton(0) && canShoot <= shootTimer)
         {
-            Debug.Log("shoot");
+            //flashTimer = 0f;
+            //muzzleFlashX = 0.1f; muzzleFlashY = 0.05f; muzzleFlashZ = 0.1f;
+
             ray = cam.ScreenPointToRay(Input.mousePosition);
             if(Physics.Raycast(ray, out hit))
             {
@@ -37,7 +54,9 @@ public class shooting : MonoBehaviour
                 }
 
             }
+            //muzzleFlash.transform.localScale = new Vector3(muzzleFlashX, muzzleFlashY, muzzleFlashZ);
             shootTimer = 0f;
         }
+
     }
 }
