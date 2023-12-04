7be6d42b4e9e2f27dcd4b66405299e11c10dddf0
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-12-04 12:54:28 +0100

    recoil: not working; gun: not going smoothly

--- Assets/scripts/cameraRotation.cs
+++ Assets/scripts/cameraRotation.cs
@@ -10,18 +10,19 @@ public class cameraRotation : MonoBehaviour
     private float yMouse;
     private float xRotation;
     public float speed = 700f;
-//aiming
+    //aiming
     public Camera cam;
-    public float zoomMultiplier = 1.666667f;
-    public float defaultFov = 90;
-    public float zoomDuration = 0.1f;
-
+    private float zoomMultiplier = 1.666667f;
+    private float defaultFov = 90;
+    private float zoomDuration = 0.1f;
+    //gun aiming
+    public GameObject gun;
+    public float gunXPosition;
     //recoil
     public float recoil;
     public float recoilSpeed = 0.1f;
     public Quaternion rotation1;
     public Quaternion rotation2;
-    bool isRecoiling = false;
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +34,7 @@ public class cameraRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        gun.transform.localPosition = new Vector3(gunXPosition, -0.374f, -0.031f);
         //makes camera follow mouse
         xMouse = Input.GetAxis("Mouse X") * speed * Time.deltaTime;
         yMouse = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
@@ -45,13 +46,15 @@ public class cameraRotation : MonoBehaviour
         //zoom in
         if (Input.GetMouseButton(1))
         {
-            ZoomCamera(defaultFov / zoomMultiplier);
+            ZoomCamera(defaultFov / zoomMultiplier, -0.38f);
             speed = 400f;
+            //gunXPosition = -0.38f;
         }
         else if (cam.fieldOfView != defaultFov)
         {
-            ZoomCamera(defaultFov);
+            ZoomCamera(defaultFov, 0.221f);
             speed = 700f;
+            //gunXPosition = 0.221f;
         }
        
         if (Input.GetMouseButton(0))
@@ -70,9 +73,25 @@ public class cameraRotation : MonoBehaviour
         //Debug.Log(rotation1 + " " + rotation2 + " " + recoilSpeed);
         //xRotation = Mathf.MoveTowards(xRotation, xRotation + recoil, recoilSpeed * Time.deltaTime);
     }
-    void ZoomCamera(float target)
+    void ZoomCamera(float target, float LTarget)
     {
-        float angle = Mathf.Abs((defaultFov / zoomMultiplier) - defaultFov);
-        cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, target, angle / zoomDuration * Time.deltaTime);
+        //float angle = Mathf.Abs((defaultFov / zoomMultiplier) - defaultFov);
+        //cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, target, angle / zoomDuration * Time.deltaTime);
+        //gunXPosition = Mathf.MoveTowards(gunXPosition, LTarget, angle / zoomDuration * Time.deltaTime);
+
+        //float angle = Mathf.Abs((defaultFov / zoomMultiplier) - defaultFov);
+
+        //// Smoothly interpolate camera field of view
+        //cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, target, angle / zoomDuration * Time.deltaTime);
+
+        //// Calculate the percentage of completion for the interpolation
+        //float t = 1.0f - (cam.fieldOfView - defaultFov) / (target - defaultFov);
+
+        //// Smoothly interpolate a separate variable for gun position based on the percentage of completion
+        //float smoothGunXPosition = Mathf.Lerp(gunXPosition, LTarget, t);
+        
+        //gun.transform.localPosition = new Vector3(smoothGunXPosition, -0.374f, -0.031f);
+        
+       
     }
 }
