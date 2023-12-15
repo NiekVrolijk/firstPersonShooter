a1b1a3bdaf818e0c94c8062d4d56e07f940c99b7
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-12-15 09:44:12 +0100

    lesssgooooo

--- Assets/scripts/objectives.cs
+++ Assets/scripts/objectives.cs
@@ -10,6 +10,10 @@ public class objectives : MonoBehaviour
     private float yPosition;
     private float zPosition;
     private float goingDownSpeed = 2f;
+    private float goingUpSpeed = 1f;
+
+    private float timer;
+    private float goUpTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,15 +27,23 @@ public class objectives : MonoBehaviour
     // Update is called once per frame
     void Update()
     { //aaaahhhhhhh
-        if ( player.position.y > transform.position.y //&&
-             //player.position.x <= transform.position.x + 10f &&
-             //player.position.z <= transform.position.x + 10f && 
-             //player.position.x >= transform.position.x - 10f &&
-             //player.position.z >= transform.position.x - 10f
+        timer += Time.deltaTime;
+        if (player.position.y > transform.position.y &&
+             player.position.x <= transform.position.x + 10f &&
+             player.position.x >= transform.position.x - 10f &&
+             player.position.z <= transform.position.z + 10f &&
+             player.position.z >= transform.position.z - 10f
              )
         {
-            yPosition = Mathf.Clamp(yPosition, 0f, 20f);
+            yPosition = Mathf.Clamp(yPosition, 0.5f, 20f);
             yPosition = yPosition - goingDownSpeed * Time.deltaTime;
+            transform.localPosition = new Vector3(xPosition, yPosition, zPosition);
+            timer = 0f;
+        }
+        else if (timer >= goUpTime)
+        {
+            yPosition = Mathf.Clamp(yPosition, 0.5f, 20f);
+            yPosition = yPosition + goingUpSpeed * Time.deltaTime;
             transform.localPosition = new Vector3(xPosition, yPosition, zPosition);
         }
         
